06b59c4977fcf877dbc34c1797a7d278d2cda115
SavagePixie <43502485+SavagePixie@users.noreply.github.com>
2020-04-09 11:37:25 +0200

    rewritten using sanctuary

--- src/lib/asteroid.js
+++ src/lib/asteroid.js
@@ -1,18 +1,15 @@
 'use strict'
+const S = require('sanctuary')
+
 const request = require('lib/request')
 
-const writeInfo = asteroid =>
-`${asteroid.name} will be approaching Earth on ${asteroid.approachDate} at a speed of ${asteroid.speed.toFixed(2)} km/s.
-It will be ${asteroid.distance.toFixed(1)} kilometers away from Earth.
-It is${asteroid.dangerous ? ' ' : ' not '}potentially dangerous.
-Its estimated diameter is between ${asteroid.eDiameter.min.toFixed(2)} and ${asteroid.eDiameter.max.toFixed(2)} meters.
-For more info, check ${asteroid.url}`
+const getAsteroidInfo = num => S.pipe([
+	S.take(num),
+	S.fromMaybe([]),
+	S.map(writeInfo),
+])
 
-const getAsteroidInfo = num => arr => arr
-	.slice(0, num)
-	.map(writeInfo)
-
-const parseAsteroids = obj => obj.map(x => ({
+const parseAsteroids = S.map(x => ({
 	name: x.name,
 	approachDate: x.close_approach_data[0].close_approach_date_full,
 	dangerous: x.is_potentially_hazardous_asteroid,
@@ -30,9 +27,18 @@ const parseResponse = ({ near_earth_objects: objs }) => Object
 	.flatMap(x => parseAsteroids(objs[x]))
 	.sort((a, b) => a.distance - b.distance)
 
+const writeInfo = asteroid =>
+	`${asteroid.name} will be approaching Earth on ${asteroid.approachDate} at a speed of ${asteroid.speed.toFixed(2)} km/s.
+	It will be ${asteroid.distance.toFixed(1)} kilometers away from Earth.
+	It is${asteroid.dangerous ? ' ' : ' not '}potentially dangerous.
+	Its estimated diameter is between ${asteroid.eDiameter.min.toFixed(2)} and ${asteroid.eDiameter.max.toFixed(2)} meters.
+	For more info, check ${asteroid.url}`
+
 module.exports = ({ host, path, key }) => (start, end) => request({
 	host,
 	path: `${path}?start_date=${start}&end_date=${end}&api_key=${key}`
-}).then(({ body }) => JSON.parse(body))
-	.then(parseResponse)
-	.then(getAsteroidInfo(1))
+}).then(({ body }) => S.pipe([
+	JSON.parse,
+	parseResponse,
+	getAsteroidInfo(1)
+])(body))
