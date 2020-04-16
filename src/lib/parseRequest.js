67f9914c5fcbc4eeeda04717f9d517d08acc0eeb
SavagePixie <43502485+SavagePixie@users.noreply.github.com>
2020-04-16 11:39:00 +0200

    fuctional approach to parseRequest. Added check for date

--- src/lib/parseRequest.js
+++ src/lib/parseRequest.js
@@ -1,20 +1,37 @@
 'use strict'
+const Either = require('lib/Either')
+
+const dateEx = /(?:on)?\s(\w{1,4}(?:-|\/|\s)\w{1,3}(?:-|\/|\s)\d{1,4})/
 
 const formatMonth = month => (month + 1).toString().padStart(2, '0')
 const formatDate = date => `${date.getFullYear()}-${formatMonth(date.getMonth())}-${date.getDate()}`
 
 const checkKeyWords = str => str.includes('asteroid') && str.includes('close')
+	? Either.Right(str)
+	: Either.Left(null)
 
-module.exports = str => {
-	if (checkKeyWords(str)) {
-		return ({
-			result: 'ok',
-			payload: {
-				start: formatDate(new Date()),
-				end: formatDate(new Date()),
-			}
-		})
-	} else {
-		return ({ result: 'notOk' })
-	}
-}
+const matchDate = str => dateEx.test(str)
+	? str.match(dateEx)[1]
+	: Date.now()
+
+const parseDate = str => new Date(str) == 'Invalid Date'
+	? Either.Left(null)
+	: Either.Right(new Date(str))
+
+const buildObject = date => ({
+	start: formatDate(date),
+	end: formatDate(date),
+})
+
+module.exports = str =>
+	checkKeyWords(str)
+		.map(matchDate)
+		.chain(parseDate)
+		.map(buildObject)
+		.fold(
+			() => ({ result: 'notOk' }),
+			x => ({
+				result: 'ok',
+				payload: x,
+			})
+		)
